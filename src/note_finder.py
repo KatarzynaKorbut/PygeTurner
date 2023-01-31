f0db4f4bd7ae2e1742f87d269e63d1491822a776
Katarzyna Korbut <korbutkasia@gmail.com>
2023-01-31 05:12:39 +0000

    Dalsze usprawnienia wyszukiwania położenia

--- src/note_finder.py
+++ src/note_finder.py
@@ -1,44 +1,70 @@
 import logging
 from collections import deque
-from typing import Deque, Iterable, Sequence
+from typing import Deque, Iterable, List, Tuple
 
 from fuzzysearch import Match, find_near_matches
 from music21.note import Note
+from music21.stream.iterator import StreamIterator
 
 
 class NoteFinder:
-    def __init__(self, sheet_notes: Sequence[Note]):
-        self.sheet_notes = sheet_notes
-        self.next_offset = 0
-        self.note_history: Deque[Note] = deque(maxlen=5)
-
-    @classmethod
-    def matches(cls, note_a: Note, note_b: Note):
-        return note_a.name == note_b.name
-
-    def jump_distance(self, match: Match):
-        return abs(match.end - self.next_offset)
+    MIN_NOTE_HISTORY = 3
+    MAX_NOTE_HISTORY = 6
 
     @staticmethod
-    def note_names(note_list: Iterable[Note]):
-        return [n.name for n in note_list]
+    def notes_to_pitch_classes(notes: Iterable[Note]) -> List[int]:
+        return [note.pitch.pitchClass for note in notes]
 
-    def locate(self, new_note: Note) -> Note:
+    def __init__(self, sheet_notes: StreamIterator[Note]):
+        self.sheet_notes = sheet_notes
+        self.sheet_note_pitch_classes = self.notes_to_pitch_classes(self.sheet_notes)
+        self.next_offset = 0
+        self.note_history = deque(maxlen=self.MAX_NOTE_HISTORY)
+
+    @classmethod
+    def notes_match(cls, note_a: Note, note_b: Note):
+        return note_a.pitch.pitchClass == note_b.pitch.pitchClass
+
+    def jump_distance(self, match: Match) -> float:
+        match_last_note = self.sheet_notes[match.end - 1]
+        match_last_note_position = match_last_note.getOffsetInHierarchy(
+            self.sheet_notes.srcStream
+        )
+        next_note = self.sheet_notes[self.next_offset]
+        next_note_position = next_note.getOffsetInHierarchy(self.sheet_notes.srcStream)
+
+        beat_distance = float(match_last_note_position - next_note_position)
+        return abs(beat_distance)
+
+    def match_penalty(self, match: Match) -> float:
+        JUMP_FACTOR = 1 / 20
+        return match.dist + self.jump_distance(match) * JUMP_FACTOR
+
+    def locate(self, new_note: Note) -> Tuple[Note, float, float]:
         self.note_history.append(new_note)
 
         next_note = self.sheet_notes[self.next_offset]
-        if self.matches(new_note, next_note):
+        if len(self.note_history) < self.MIN_NOTE_HISTORY:
             self.next_offset += 1
-            return next_note
+            return next_note, -1, 0
         else:
-            note_history = list(self.note_history)
-            matches = find_near_matches(
-                self.note_names(note_history),
-                self.note_names(self.sheet_notes),
-                max_l_dist=len(note_history) - 1)
-            matches = sorted(matches, key=lambda m: (m.dist, self.jump_distance(m)))
-            best_match = matches[0]
-            logging.debug("NoteFinder best match: %s", best_match)
+            note_history_pitch_classes = self.notes_to_pitch_classes(self.note_history)
+            matches = list(
+                find_near_matches(
+                    note_history_pitch_classes,
+                    self.sheet_note_pitch_classes,
+                    max_l_dist=len(note_history_pitch_classes) // 3,
+                )
+            )
+            if len(matches) == 0:
+                self.next_offset += 1
+                return next_note, -1, 0
+
+            best_match = min(matches, key=lambda m: self.match_penalty(m))
+            penalty = self.match_penalty(best_match)
+            jump = self.jump_distance(best_match)
+
             self.next_offset = best_match.end
-            next_note = self.sheet_notes[best_match.end - 1]
-            return next_note
+            next_note = self.sheet_notes[self.next_offset - 1]
+
+            return next_note, penalty, jump
